d44cf439a7aa148c0d970e1b3e5eca7b6e13b142
Larry <B>
2019-08-20 09:45:00 -0700

    Fist commit with open book api and review

---
+++ ebooks/api/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from ebooks.models import Ebook, Review
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Review
+        exclude = ("ebook",)
+
+
+class EbookSerializer(serializers.ModelSerializer):
+    reviews = ReviewSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Ebook
+        fields = "__all__"
